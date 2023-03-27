1336ef176855bd8cd3b4edb1920e650d7b1bb51f
zaher1307 <ahmedzaher1307@gmail.com>
2023-03-27 17:02:51 +0200

    refactor: modify auth err handler middleware

--- src/middlewares/authentication-error-handler.js
+++ src/middlewares/authentication-error-handler.js
@@ -4,9 +4,8 @@ function authenticationErrorHandler(err, req, res, next) {
   log.debug('logging in auth failed')
   if (err.status === 401) {
     return res.status(err.status).json(err.message)
-  } else {
-    next()
   }
+  next(err)
 }
 
 module.exports = authenticationErrorHandler
